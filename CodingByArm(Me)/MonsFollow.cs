029aa1250ce39c7c5153cf824ba22551029731cd
ArmNewbyDev <92511077+ArmNewbyDev@users.noreply.github.com>
2023-03-12 13:49:15 +0700

    Update MonsFollow.cs

--- CodingByArm(Me)/MonsFollow.cs
+++ CodingByArm(Me)/MonsFollow.cs
@@ -6,11 +6,6 @@ public class MonsFollow : MonoBehaviour
 {
     [SerializeField] Enemy enemy;
     [SerializeField] bool IsEnemyFollow = false;
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
